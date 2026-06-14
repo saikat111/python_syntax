c11d03107708a0a5f873f19c40a2c42c7b54c37a
saikat111 <s8856010@gmail.com>
2026-06-14 22:16:57 +0600

    basic print

--- print.py
+++ print.py
@@ -1,1 +1,36 @@
 print("Hello, World!")
+x = 10
+print(x)
+#type casting
+y = 11
+print(str(y))
+#Many Values to Multiple Variables
+a, b, c = "Orange", "Banana", "Cherry"
+print(a)    
+print(b)
+print(c)
+#One Value to Multiple Variables
+x = y = z = "Orange"
+print(x)
+
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+
+x = '5'
+y = '10'
+print(x + y)
+
+#type function user for checking the data type of a variable
+print(type(x))
+
+
